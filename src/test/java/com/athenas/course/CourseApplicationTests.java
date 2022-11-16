d661b1513c4adf1b35d2ddaa1bd8c7e785963f7f
marcos2312 <marcos.rcomercial@gmail.com>
2022-11-16 14:10:52 -0300

    Project created

---
+++ src/test/java/com/athenas/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athenas.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
